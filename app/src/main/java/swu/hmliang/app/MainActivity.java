c3e44acb38d1d35f4884cacca89b3cc7b5062b7e
ml-hu <2425066241@qq.com>
2020-07-12 10:31:53 +0800

    1.init工程

---
+++ app/src/main/java/swu/hmliang/app/MainActivity.java
@@ -0,0 +1,14 @@
+package swu.hmliang.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
